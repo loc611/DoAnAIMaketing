9b823440e354bf2df4f432140fc276402586f641
loc611 <2500114939@vanlangsaigon.edu.vn>
2026-08-14 20:36:10 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@imga': 'D:/BaiTapAIMaketing/AI3D/imga'
+      '@imga': path.resolve(__dirname, './src/assets/imga')
     }
   },
   server: {
